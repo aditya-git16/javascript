e141a21654f4be3c70f4b7f44285f0bbe4ee5185
adi16an <118093307+adi16an@users.noreply.github.com>
2024-01-29 15:42:44 +0530

    2nd

---
+++ week1.4/anagram.js
@@ -0,0 +1,29 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+
+  Once you've implemented the logic, test your code by running
+  - `npm run test-anagram`
+*/
+
+function isAnagram(str1, str2) {
+  if (str1.sort == str2.sort) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+let ans = isAnagram('spar', 'rasp');
+console.log(ans);
+module.exports = isAnagram;
+
+
+// sorting a string in js
+
+console.log('spar'.split('').sort().join(''));
+console.log('spar'.sort()); // this will not work as sort is a method of array not string
+
